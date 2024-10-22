0c30d10cfbdcd470776c0508184ce684a02a7343
arghya775 <arghyvyas775@gmail.com>
2024-10-21 21:44:15 -0400

    cleaning code

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,6 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
-        yvgray: "var(--yvgray)",
         textColour: "var(--textColour)",
       },
     },
